b24fb88225c4ac80e7482a02401a8e95cc72358c
XII012 <zkw374628354@gmail.com>
2020-05-31 17:45:50 +0800

    Revert "lessson3 部署" 更新了setting的ALLOWED_HOSTS；修改了.gitignore This reverts commit 293b6832

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v6$545hdpud^brjuemm3yf)nez+_yc5r=kz5trv#-=z$kvr$2z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['XII012.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
